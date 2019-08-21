b9e3e2528c626fe57f53e63156373a1ed47a20a7
Afraz Ahmadzadeh <afrazkhan@gmail.com>
2019-08-21 12:23:52 +0200

    Adding ec2 listing module

--- opstools/main.py
+++ opstools/main.py
@@ -19,13 +19,17 @@ def main():
         """,
         add_help=False
     )
-    main_parser.add_argument("command", help="The subcommand to run", choices=["allowme"])
+    main_parser.add_argument("command", help="The subcommand to run", choices=["allowme", "ec2ls"])
 
     args, subc_args = main_parser.parse_known_args()
 
     if args.command == "allowme":
         import opstools.aws.allow_me as allow_me
         allow_me.main(subc_args)
+
+    if args.command == "ec2ls":
+        import opstools.aws.ec2_list as ec2_list
+        ec2_list.main()
 
 if __name__ == "__main__":
     main()
