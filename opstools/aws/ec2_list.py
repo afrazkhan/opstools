289f1c2663e7edeef16d3008a2f2c6564ad31e9b
Afraz Ahmadzadeh <afrazkhan@gmail.com>
2021-09-03 12:53:47 +0200

    Fixed ec2 instance listing

--- opstools/aws/ec2_list.py
+++ opstools/aws/ec2_list.py
@@ -14,17 +14,24 @@ def get_listing():
     """ Return a listing for EC2 instances """
 
     ec2 = boto3.client('ec2')
+    listing = []
 
     try:
-        listing = ec2.describe_instances(Filters=[
-            {
-                'Name': 'instance-state-name',
-                'Values': [ 'running' ]
-            }
-        ])
+        full_listing = ec2.describe_instances(
+            Filters=[
+                {
+                    'Name': 'instance-state-name',
+                    'Values': [ 'running' ]
+                }
+            ],
+            MaxResults=1000)
     except botocore.exceptions.ClientError as e:
         print(e)
         sys.exit(1)
+
+    for reservation in full_listing['Reservations']:
+        for instance in reservation['Instances']:
+            listing.append(instance)
 
     return listing
 
@@ -36,17 +43,17 @@ def extract_interesting_keys(listing):
 
     simplified_listing = []
 
-    for instance in listing['Reservations']:
+    for instance in listing:
         try:
-            name = next(n for n in instance['Instances'][0]['Tags'] if n['Key'] == 'Name')['Value']
+            name = next(n for n in instance['Tags'] if n['Key'] == 'Name')['Value']
         except (KeyError, StopIteration):
-            name = instance['Instances'][0]['InstanceId']
+            name = instance['InstanceId']
 
-        instance_id = instance['Instances'][0]['InstanceId']
-        private_ip = instance['Instances'][0]['NetworkInterfaces'][0]['PrivateIpAddress']
+        instance_id = instance['InstanceId']
+        private_ip = instance['NetworkInterfaces'][0]['PrivateIpAddress']
 
         try:
-            public_ip = instance['Instances'][0]['NetworkInterfaces'][0]['Association']['PublicIp']
+            public_ip = instance['NetworkInterfaces'][0]['Association']['PublicIp']
         except KeyError:
             public_ip = "None"
 
