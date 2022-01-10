0149d3a92297739554242384acd0977caa615181
Afraz Ahmadzadeh <afrazkhan@gmail.com>
2022-01-10 14:48:58 +0100

    WIP: Using multiprocessing instead

--- opstools/url/stresstest.py
+++ opstools/url/stresstest.py
@@ -14,7 +14,7 @@ import argparse
 import time
 import urllib3
 import requests
-import threading
+from multiprocessing import Process
 import sys
 
 
@@ -23,8 +23,16 @@ def refresh_line(output):
     Refreshes the output line to show the stress testing working
     """
 
-    sys.stdout.write(f"\r{output}")
     sys.stdout.flush()
+    sys.stdout.write(f"\r{output}")
+
+def write_log(output):
+    """
+    Log output (of failed connections, but can be anything) to errors.log
+    """
+
+    with open("errors.log", "a", encoding="UTF-8") as log_file:
+        log_file.write(output)
 
 def send_requests(url, rate, codes, session=None):
     """
@@ -32,7 +40,6 @@ def send_requests(url, rate, codes, session=None):
     is supplied
     """
 
-    fresh_sessions = False
     this_session = session
     these_headers = {}
     error_counter = 0
@@ -42,18 +49,27 @@ def send_requests(url, rate, codes, session=None):
         time.time()
 
         if not session:
-            fresh_sessions = True
             this_session = requests.session()
             this_session.keep_alive = False
             these_headers={"Connection": "close"}
 
-        this_request = this_session.get(url, verify=False, headers=these_headers)
+        try:
+            this_request = this_session.get(url, verify=False, headers=these_headers)
+        except requests.exceptions.ConnectionError as e:
+            print(f"Could not make initial connection: {e}")
+            sys.exit(0)
         status = this_request.status_code
 
         if status not in codes:
             error_counter += 1
             last_error_time = time.ctime()
-        #     print(f"ERROR: {status}, Fresh sessions: {fresh_sessions}\nHEADERS: {this_request.headers}")
+            write_log(f"""
+Time: {last_error_time}
+Error Number: {error_counter}
+Connection Type: {this_request.headers['Connection']}
+Full Headers: {this_request.headers}
+
+            """)
 
         refresh_line(f"Last Error: {last_error_time} — Total Errors: {error_counter}")
         time.sleep(rate)
@@ -99,13 +115,16 @@ def main(subc_args=None):
     else:
         codes = [200]
 
-    print(f"Requests will be sent every {rate} seconds. There will only be output if the status is not in the list provided with --codes (default is 200). Kill me with CTRL+C when you're done\n")
+    print(f"Requests will be sent every {rate} seconds. Kill me with CTRL+C when you're done\n")
     for i in range(int(args.processes)):
-        new_sessions_thread = threading.Thread(target=send_requests, args=(url, rate, codes,))
+        new_sessions_thread = Process(target=send_requests, args=(url, rate, codes,))
         new_sessions_thread.start()
+        print(f"Started new_session thread {i + 1}")
 
-        reused_sessions_thread = threading.Thread(target=send_requests, args=(url, rate, codes, requests.session(),))
+        reused_sessions_thread = Process(target=send_requests, args=(url, rate, codes, requests.session(),))
         reused_sessions_thread.start()
+        print(f"Started reused_session thread {i + 1}")
+
 
 if __name__ == "__main__":
     main()
